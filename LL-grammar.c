387c44200e3e3db505aa60524c851c84b130638d
Sabína Gregušová <sabina.gregusova@gmail.com>
2018-10-30 20:49:36 +0100

    Adding the grammar rules

--- LL-grammar.c
+++ LL-grammar.c
@@ -1,3 +1,11 @@
+/*
+IFJ 2018
+Adam Hostin, xhosti02
+Sabína Gregušová, xgregu02
+Dominik Peza, xpezad00
+Adrián Tulušák, xtulus00
+*/
+
 #include "LL-grammar.h"
 
 int get_rule_num(int nonterminal, Token_t* token){
@@ -7,66 +15,154 @@ int get_rule_num(int nonterminal, Token_t* token){
                 if(token->attr.keyword == KEYWORD_DEF)
                     return 1;
                 else if(token->attr.keyword == KEYWORD_IF)
-                    return 5;
+                    return 4;
                 else if(token->attr.keyword == KEYWORD_WHILE)
-                    return 5;
+                    return 4;
                 else if(token->attr.keyword == KEYWORD_PRINT)
-                    return 5;
+                    return 4;
                 else if(token->attr.keyword == KEYWORD_LENGTH)
-                    return 5;
+                    return 4;
                 else if(token->attr.keyword == KEYWORD_SUBSTR)
-                    return 5;
+                    return 4;
                 else if(token->attr.keyword == KEYWORD_ORD)
-                    return 5;
+                    return 4;
                 else if(token->attr.keyword == KEYWORD_CHR)
-                    return 5;
+                    return 4;
+                else if(token->attr.keyword == KEYWORD_INPUTS)
+                    return 4;
+                else if(token->attr.keyword == KEYWORD_INPUTI)
+                    return 4;
+                else if(token->attr.keyword == KEYWORD_INPUTF)
+                    return 4;
             }
             else if(token->token == TYPE_EOL)
                 return 2;
             else if(token->token == TYPE_IDENTIFIER)
-                return 3;
-            else if(token->token == TYPE_EOF)
                 return 4;
+            else if(token->token == TYPE_EOF)
+                return 3;
             return 0;
             break;
         case STATEMENT:
-            if(token->token == TYPE_EOL)
-                return 17;
-            else if(token->token == TYPE_KEYWORD){
+            if(token->token == TYPE_KEYWORD){
                 if(token->attr.keyword == KEYWORD_IF)
-                    return 10;
+                    return 5;
                 else if(token->attr.keyword == KEYWORD_WHILE)
-                    return 11;
+                    return 6;
                 else if(token->attr.keyword == KEYWORD_PRINT)
-                    return 12;
+                    return 7;
                 else if(token->attr.keyword == KEYWORD_LENGTH)
-                    return 13;
+                    return 8;
                 else if(token->attr.keyword == KEYWORD_SUBSTR)
-                    return 14;
+                    return 9;
                 else if(token->attr.keyword == KEYWORD_ORD)
-                    return 15;
+                    return 10;
                 else if(token->attr.keyword == KEYWORD_CHR)
-                    return 16;
+                    return 11;
+                else if(token->attr.keyword == KEYWORD_INPUTS)
+                    return 12;
+                else if(token->attr.keyword == KEYWORD_INPUTI)
+                    return 13;
+                else if(token->attr.keyword == KEYWORD_INPUTF)
+                    return 14;
             }
+            else if(token->token == TYPE_IDENTIFIER)
+                return 15;
             return 0;
             break;
+
+        case DECLARE:
+            if(token->token == TYPE_EOL)
+                return 20;
+            else if(token->token == TYPE_IDENTIFIER)
+                return 16;
+            //else if() // func... // TODO
+            else if(token->token == TYPE_INT)
+                return 17;
+            else if(token->token == TYPE_FLOAT)
+                return 17;
+            else if(token->token == TYPE_STRING)
+                return 17;
+            break;
+
         case VALUE:
             if(token->token == TYPE_INT)
-                return 6;
+                return 29;
             else if(token->token == TYPE_STRING)
-                return 7;
+                return 30;
             else if(token->token == TYPE_STRING)
-                return 8;
+                return 31;
             return 0;
             break;
-        case VARIABLE:
+
+        case PARAMS:
+            if(token->token == TYPE_IDENTIFIER)
+                return 21;
+            else if(token->token == TYPE_RIGHT_BRACKET)
+                return 22;
+            return 0;
             break;
-        case DECLARE:
+
+        case PARAM:
+            if(token->token == TYPE_COMMA)
+                return 23;
+            else if(token->token == TYPE_RIGHT_BRACKET)
+                return 24;
+            return 0;
             break;
+
+        case ARGVS:
+            if(token->token == TYPE_INT)
+                return 29;
+            else if(token->token == TYPE_FLOAT)
+                return 30;
+            else if(token->token == TYPE_STRING)
+                return 31;
+            return 0;
+            break;
+
+        case ARGV:
+            if(token->token == TYPE_COMMA)
+                return 27;
+            else if(token->token == TYPE_RIGHT_BRACKET)
+                return 28;
+            return 0;
+        
     }
         
 }
 
-void get_rule(){
-    ;
+void get_rule(int rule_num){
+    switch (rule_num){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 12:
+            break;
+        case 13:
+            break;
+        case 14:
+            break;
+        case 15:
+            break;
+    }
 }
--- LL-grammar.h
+++ LL-grammar.h
@@ -1,11 +1,23 @@
-#define PROG 1
-#define VALUE 2
-#define STATEMENT 3
-#define VARIABLE 4
-#define DECLARE 5
-#define UNDEFINED 6
+/*
+IFJ 2018
+Adam Hostin, xhosti02
+Sabína Gregušová, xgregu02
+Dominik Peza, xpezad00
+Adrián Tulušák, xtulus00
+*/
 
-#include "lexer.h"
+#define PROG 1
+#define STATEMENT 2
+#define DECLARE 3
+#define PARAMS 4
+#define PARAM 5
+#define ARGVS 6
+#define ARGV 7
+#define VALUE 8
+
+#define NOT_FOUND 0
+
+//#include "lexer.h"
 
 int get_rule_num(int nonterminal, Token_t* token);
 void get_rule();
